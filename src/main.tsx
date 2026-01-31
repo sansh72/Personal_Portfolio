6504486b9ab1bf538041ea8404400296a842c14f
Sansh <sanshraysinghlangeh@gmail.com>
2026-02-01 05:11:48 +0530

    Added analytics

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { AuthProvider } from './contexts/AuthContext'
+import { Analytics } from '@vercel/analytics/react'
 import './index.css'
 import App from './App'
 
@@ -9,6 +10,7 @@ createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
       <AuthProvider>
+        <Analytics />
         <Routes>
           <Route path="/" element={<App />} />
           <Route path="/:username" element={<App />} />
